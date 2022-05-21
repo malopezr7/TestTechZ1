25cacaaab2a86a82d82ffba52e61da0634a8eb35
Manuel Lopez <mlopez@activalaagencia.com>
2022-05-21 21:07:06 +0200

    .

--- src/components/styles/Home.styles.js
+++ src/components/styles/Home.styles.js
@@ -4,6 +4,6 @@ import styled from 'styled-components/native';
 export const HomeView = styled.SafeAreaView`
   justify-content: flex-start;
   align-items: center;
-  background-color: grey;
+  background-color: rebeccapurple;
   flex: 1;
 `;
